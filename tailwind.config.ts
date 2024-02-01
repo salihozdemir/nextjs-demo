0c4d336a19c46eaffecc7bdca6b030d294289e47
Salih <salihozdemir94@gmail.com>
2024-02-01 15:31:19 +0300

    chore: update the container size

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,9 +13,8 @@ const config = {
   theme: {
     container: {
       center: true,
-      padding: "2rem",
       screens: {
-        "2xl": "1400px",
+        "2xl": "1200px",
       },
     },
     extend: {
